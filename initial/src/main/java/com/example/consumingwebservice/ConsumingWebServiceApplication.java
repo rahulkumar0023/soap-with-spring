75cc6a0074d5fc2a8f47dfbf13cd4af6d7b516a0
Rahul <rahularya0083@gmqil.com>
2023-05-04 18:47:03 +0530

    SOAP client

--- initial/src/main/java/com/example/consumingwebservice/ConsumingWebServiceApplication.java
+++ initial/src/main/java/com/example/consumingwebservice/ConsumingWebServiceApplication.java
@@ -1,7 +1,10 @@
 package com.example.consumingwebservice;
 
+import com.example.consumingwebservice.wsdl.GetCountryResponse;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ConsumingWebServiceApplication {
@@ -10,4 +13,17 @@ public class ConsumingWebServiceApplication {
 		SpringApplication.run(ConsumingWebServiceApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner lookup(CountryClient quoteClient) {
+		return args -> {
+			String country = "Spain";
+
+			if (args.length > 0) {
+				country = args[0];
+			}
+			GetCountryResponse response = quoteClient.getCountry(country);
+			System.err.println(response.getCountry().getCapital());
+			System.err.println(response.getCountry().getCurrency());
+		};
+	}
 }
